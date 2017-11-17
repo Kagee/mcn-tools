dfa1e18bf5809b9e16bf22d5b0d7b9637519d881
Anders Einar Hilden <hildenae@gmail.com>
2017-11-18 00:05:45 +0100

    test/prod-switch for build_regexp.py

--- build_regexp.py
+++ build_regexp.py
@@ -1,13 +1,18 @@
 #! /usr/bin/env python3
 import re
 import urllib.request
-
+import sys
 import telnetlib
 
 def get_non_ascii():
-    resource = urllib.request.urlopen("https://www.norid.no/en/regelverk/navnepolitikk/")
-    content =  resource.read().decode(resource.headers.get_content_charset())
-    result = re.search('<a id="link3">(.*)<a id="link4">', content, re.MULTILINE|re.DOTALL)
+    if len(sys.argv) > 1 and sys.argv[1] == "--prod":
+        resource = urllib.request.urlopen("https://www.norid.no/en/regelverk/navnepolitikk/")
+        s =  resource.read().decode(resource.headers.get_content_charset())
+    else:
+        a_file = open('navnepolitikk.txt', encoding='utf-8')
+        s = a_file.read()
+
+    result = re.search('<a id="link3">(.*)<a id="link4">', s, re.MULTILINE|re.DOTALL)
     chars = []
     for c in result.group(0):
         if (ord(c) > 0xBF): # because 'NO-BREAK SPACE' (U+00A0)
@@ -21,16 +26,17 @@ def get_non_ascii():
     return r
 
 def get_cat_domains(repl):
-    whois = telnetlib.Telnet("whois.norid.no", 43)
-    whois.write("-c utf-8 NNRI4O-NORID\n".encode('ascii'))
-    result = whois.read_all()
-    whois.close()
-    s = result.decode('utf-8')
-    #a_file = open('whois.txt', encoding='utf-8')
-    #s = a_file.read()
-    #print(s)
+    if len(sys.argv) > 1 and sys.argv[1] == "--prod":
+        whois = telnetlib.Telnet("whois.norid.no", 43)
+        whois.write("-c utf-8 NNRI4O-NORID\n".encode('ascii'))
+        result = whois.read_all()
+        whois.close()
+        s = result.decode('utf-8')
+    else:
+        a_file = open('whois.txt', encoding='utf-8')
+        s = a_file.read()
+
     r = re.search('^Domains\.*: (.*)', s, re.MULTILINE)
-    #print("0: " + r.group(0))
     if r:
         domains = r.group(1).split()
         repl = get_non_ascii()
