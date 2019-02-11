bf65a3dc85312fe1ea896b4c72e98e74d9031adb
Anders Einar Hilden <hildenae@gmail.com>
2019-02-11 17:03:29 +0100

    better tool checking

--- make-mcn/01-check-tools.sh
+++ make-mcn/01-check-tools.sh
@@ -8,9 +8,13 @@ has idn
 if [ ! -d "./massdns" ]; then
   git clone https://github.com/blechschmidt/massdns.git
 else
-  cd ./massdns && git pull
+  cd ./massdns && git pull 1>/dev/null
 fi
 # Hacky compile-only-if-new, since massdns has a sub-optimal Makefile
 find . ! -path '*.git*'  -newer bin/massdns | egrep '.*' >/dev/null && make
 cd ..
-echo "[INFO] Found massdns"
+if [ -f massdns/bin/massdns ]; then
+  echo "[INFO] Found massdns"
+else
+   echo "[ERROR] Didn't find $1, please check source"; exit 1;
+fi
