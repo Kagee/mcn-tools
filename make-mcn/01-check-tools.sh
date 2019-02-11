5eeb808910d1d2a3845e3e3c8ce55036fa4ff7a7
Anders Einar Hilden <hildenae@gmail.com>
2019-02-11 16:29:26 +0100

    Simple script for checking for tools we use

---
+++ make-mcn/01-check-tools.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+function has {
+  command -v $1 >/dev/null && echo "[INFO] Found $1" || { echo "[ERROR] Didn't find $1, please install"; exit 1; }
+}
+has git
+has idn
+
+if [ ! -d "./massdns" ]; then
+  git clone https://github.com/blechschmidt/massdns.git
+else
+  cd ./massdns && git pull
+fi
+# Hacky compile-only-if-new, since massdns has a sub-optimal Makefile
+find . ! -path '*.git*'  -newer bin/massdns | egrep '.*' >/dev/null && make
+cd ..
+echo "[INFO] Found massdns"
